2bfcd118656a55c31870694f1ac53cc881b675f7
Carlo <carlo.bene@web.de>
2022-07-04 23:37:42 +0200

    Github icon with hover effect and link on mouse down

--- EngineSigma.Editor/MVVM/View/AboutUsView.xaml.cs
+++ EngineSigma.Editor/MVVM/View/AboutUsView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace EngineSigma.Editor.MVVM.View;
 
@@ -7,5 +9,26 @@ public partial class AboutUsView : UserControl
     public AboutUsView()
     {
         InitializeComponent();
+    }
+
+    private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        var psi = new ProcessStartInfo()
+        {
+            FileName = "https://github.com/jonathan-knobling",
+            UseShellExecute = true
+        };
+        Process.Start(psi);
+    }
+
+    private void UIElement_OnMouseDown1(object sender, MouseButtonEventArgs e)
+    {
+        
+        var psi = new ProcessStartInfo()
+        {
+            FileName = "https://github.com/Carloo999",
+            UseShellExecute = true
+        };
+        Process.Start(psi);
     }
 }
